43d7dadb1788ddb0b88214a2b396f316aae68299
lironeizenman@gmail.com <lironeizenman@gmail.com@a0b06e45-fb3a-aa8b-2ea5-c1dac06765e9>
2011-04-02 11:42:59 +0000

    added the basic game Board also added first draft of the players form

---
+++ trunk/MonopolyUI/src/services/EventHandler.java
@@ -0,0 +1,41 @@
+package services;
+
+import main.GameStates;
+import main.MonopolyGame;
+import main.StateManager.GameStateChangedEvent;
+import main.StateManager.GameStateChangedEventListener;
+
+/**
+ * 
+ * @author Benda & Eizenman
+ * Handles all events triggered from the logic
+ * In this case registered only to the base event listener to log all the messages to the console
+ * 
+ */
+public class EventHandler
+{
+	/**
+	 * Registers to the events in the logic
+	 * @param monopolyGame - the logic
+	 */
+	public void registerEvents(MonopolyGame monopolyGame)
+	{
+		monopolyGame.getStateManager().registerToGameChangedEvent(GameStateChangedEventListener.class, new GameStateChangedEventListener() {
+			
+			@Override
+			public void gameStateChanged(GameStateChangedEvent evt) {
+				writeNewState(evt.getNewState(), evt.getMessage());
+			}
+		});
+	}
+	
+	/**
+	 * Write the new state to the console
+	 * @param newState - the new state
+	 * @param message - the message triggered with the new state
+	 */
+	private void writeNewState(GameStates newState, String message)
+	{
+		//System.out.println(newState + ": " + message);
+	}
+}
